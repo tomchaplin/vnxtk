83d44d3f3d9b0bacdd642e9cd30776663b98b43a
Thomas Chaplin <tomrchaplin@gmail.com>
2023-02-14 12:33:32 +0000

    Test all builders

--- test.py
+++ test.py
@@ -1,8 +1,15 @@
-from vnxtk.builders import BifurcationVNetBuilder
+from vnxtk.builders import BifurcationVNetBuilder, GridVNetBuilder, VoronoiVNetBuilder
 from vnxtk.flow_models import LinearModel
 from vnxtk.builders.bifurcation import BifurcationBoundaryConditions
+from vnxtk.builders.grid import GridBoundaryConditions
 
-builder = BifurcationVNetBuilder(depth=3, thinned=False, boundary_conditions=BifurcationBoundaryConditions.CROSS_LOAD)
+# builder = BifurcationVNetBuilder(
+#    depth=3, thinned=False, boundary_conditions=BifurcationBoundaryConditions.CROSS_LOAD
+# )
+# builder = GridVNetBuilder(size=5, boundary_conditions=GridBoundaryConditions.CROSS_LOAD)
+builder = VoronoiVNetBuilder(n_points=50)
 V = builder()
 model = LinearModel()
 V.model(model)
+fig = V.get_interactive_graph_viewer(cone_key="speed")
+fig.show()
