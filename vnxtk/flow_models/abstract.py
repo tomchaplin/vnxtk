a3978f32ea76c5e01687d381d156625283fb99ec
Thomas Chaplin <tomrchaplin@gmail.com>
2023-02-14 12:49:15 +0000

    Black formatting

--- vnxtk/flow_models/abstract.py
+++ vnxtk/flow_models/abstract.py
@@ -1,6 +1,7 @@
 from abc import ABC, abstractmethod
 import networkx as nx
 
+
 class VNetModel(ABC):
     @abstractmethod
     def __call__(self, underlying: nx.Graph) -> nx.DiGraph:
--- vnxtk/flow_models/linear.py
+++ vnxtk/flow_models/linear.py
@@ -7,52 +7,65 @@ import pdb
 
 def _get_edge_idx(edge, edge_basis):
     sorted_edge = sorted(edge)
-    idxs = [ idx for idx, (e, res) in enumerate(edge_basis) if e == sorted ]
+    idxs = [idx for idx, (e, res) in enumerate(edge_basis) if e == sorted]
     return idxs[0]
+
 
 # Matrix of equations describing current in = current out at each node
 def _build_first_law_eqs(underlying, edge_basis):
-    M = np.array([
+    M = np.array(
         [
-            1 if edge[1] == node else -1 if edge[0] == node else 0
-            for edge,res in edge_basis
+            [
+                1 if edge[1] == node else -1 if edge[0] == node else 0
+                for edge, res in edge_basis
+            ]
+            for node in underlying.nodes
         ]
-        for node in underlying.nodes
-    ])
-    b = np.array([ data['external_current'] for node,data in underlying.nodes(data=True)])
+    )
+    b = np.array(
+        [data["external_current"] for node, data in underlying.nodes(data=True)]
+    )
     return M, b
 
+
 def _build_cycle_row(cycle, edge_basis):
-    cycle_in_edges = list(zip( cycle, cycle[1:]+[cycle[0]] ))
-    row = np.array([
-        res if edge in cycle_in_edges else -res if (edge[1], edge[0]) in cycle_in_edges else 0
-        for edge, res in edge_basis
-    ])
+    cycle_in_edges = list(zip(cycle, cycle[1:] + [cycle[0]]))
+    row = np.array(
+        [
+            res
+            if edge in cycle_in_edges
+            else -res
+            if (edge[1], edge[0]) in cycle_in_edges
+            else 0
+            for edge, res in edge_basis
+        ]
+    )
     return row
+
 
 # Matrix of equations describing no voltage drop around any loop
 def _build_second_law_eqs(underlying, edge_basis):
     cycle_basis = nx.cycle_basis(underlying)
-    M = np.array([
-        _build_cycle_row(cycle, edge_basis)
-        for cycle in cycle_basis
-        ])
+    M = np.array([_build_cycle_row(cycle, edge_basis) for cycle in cycle_basis])
     b = np.array([0 for _ in cycle_basis])
     return M, b
 
+
 def _build_digraph(underlying, edge_basis, x):
-    modelled = underlying.to_directed(as_view = False)
-    for (edge,res), current in zip(edge_basis, x):
+    modelled = underlying.to_directed(as_view=False)
+    for (edge, res), current in zip(edge_basis, x):
         if current > 0:
-            modelled[edge[0]][edge[1]]['current'] = current
+            modelled[edge[0]][edge[1]]["current"] = current
             modelled.remove_edge(edge[1], edge[0])
         else:
-            modelled[edge[1]][edge[0]]['current'] = -current
+            modelled[edge[1]][edge[0]]["current"] = -current
             modelled.remove_edge(edge[0], edge[1])
     for _, _, data in modelled.edges(data=True):
-        data['speed'] = data['current']/((math.pi/4.0)*data['diam']*data['diam'])
-        data['time'] = data['length']/data['speed']
-        data['weight'] = data['time']
+        data["speed"] = data["current"] / (
+            (math.pi / 4.0) * data["diam"] * data["diam"]
+        )
+        data["time"] = data["length"] / data["speed"]
+        data["weight"] = data["time"]
     return modelled
 
 
@@ -64,7 +77,7 @@ class LinearModel(VNetModel):
         modelled = nx.DiGraph()
         # List of (edge, resistance) tuples
         edge_basis = [
-            ((u, v), d['length']/((math.pi/4.0)*d['diam']*d['diam']))
+            ((u, v), d["length"] / ((math.pi / 4.0) * d["diam"] * d["diam"]))
             for u, v, d in underlying.edges(data=True)
         ]
         # Write down Kirchoff's laws
@@ -73,7 +86,6 @@ class LinearModel(VNetModel):
         M = np.vstack((M1, M2))
         b = np.hstack((b1, b2)).reshape(-1, 1)
         # Compute current in each edge
-        x,_,_,_ = np.linalg.lstsq(M, b, rcond=None)
+        x, _, _, _ = np.linalg.lstsq(M, b, rcond=None)
         # Build digraph
         return _build_digraph(underlying, edge_basis, x.flatten().tolist())
-
