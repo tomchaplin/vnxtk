947e2f24a1952d3adc4dd66a777feecec7a5857d
Thomas Chaplin <tomrchaplin@gmail.com>
2023-02-15 12:01:27 +0000

    Allow arbitrary number of inlets/outlets

--- vnxtk/alterations/random_boundary.py
+++ vnxtk/alterations/random_boundary.py
@@ -1,22 +1,21 @@
 from .abstract import VNetAlteration
 from vnxtk import VNet
 from random import sample
-from typing import Tuple
 
 
 class RandomBoundary(VNetAlteration):
-    def __init__(self, n_nodes: int = 1):
-        self.n_nodes = n_nodes
-        pass
+    def __init__(self, n_inlets: int = 1, n_outlets: int = 1):
+        self.n_inlets = n_inlets
+        self.n_outlets = n_outlets
 
     def __call__(self, V: VNet):
         node_list = list(V.underlying.nodes)
-        new_boundary = sample(node_list, 2 * self.n_nodes)
+        new_boundary = sample(node_list, self.n_inlets + self.n_outlets)
         for node, ndt in V.underlying.nodes(data=True):
             ndt["external_current"] = (
                 -1
-                if node in new_boundary[0 : self.n_nodes]
+                if node in new_boundary[0 : self.n_inlets]
                 else 1
-                if node in new_boundary[self.n_nodes :]
+                if node in new_boundary[self.n_inlets :]
                 else 0
             )
